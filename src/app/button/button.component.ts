2362feb41aa0ca78f9f643972b7317db2de6f234
kanura56@gmail.com <kanura56@gmail.com>
2023-12-11 19:29:34 +0300

    Init project

---
+++ src/app/button/button.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [MatButtonModule],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css',
+})
+export class AppButtonComponent {
+  @Input() buttonText: string = '';
+}
